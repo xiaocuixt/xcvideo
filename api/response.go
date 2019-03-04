48748781600aeca3bfc67f64ceb462a50e55d047
xiaocuixt <cjw624923@126.com>
2019-03-04 22:50:10 +0800

    初始化架构

---
+++ api/response.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "io"
+  "net/http"
+)
+
+func sendErrorResponse(w http.ResponseWriter) {
+  
+}
+
+
+func sendNormalResponse(w http.ResponseWriter) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
